b0d88c366677a8dd3cf047063d3ef61e1d6e2601
Chris Chatelain <chris@digiex.ca>
2023-12-02 12:29:04 -0600

    Switch to espidf

---
+++ main/stepper.cpp
@@ -0,0 +1,68 @@
+#include "FastAccelStepper.h"
+#include "stepper.h"
+
+Stepper::Stepper(int dirPin, int enablePin, int stepPin, uint16_t rapidSpeed): myRapidSpeed(rapidSpeed) {
+    myEngine.init();
+    FastAccelStepper* myStepper = myEngine.stepperConnectToPin(stepPin);
+    
+    if (myStepper) {
+        myStepper->setDirectionPin(dirPin);
+        myStepper->setEnablePin(enablePin);
+        myStepper->setAutoEnable(true);
+        myStepper->setDelayToDisable(200);
+        myStepper->setAcceleration(1000);
+    }
+}
+
+void Stepper::UpdateActiveSpeed() {
+  int16_t aSpeed = myUseRapidSpeed ? myRapidSpeed : myNormalSpeed;
+
+  if(myStepper->isRunning() || myStepper->isRunningContinuously()) {
+    if(myStepper->getSpeedInMilliHz() <= aSpeed + (aSpeed*0.05) && myStepper->getSpeedInMilliHz() >= aSpeed - (aSpeed*0.05)) {
+        //within 0.5% of the target speed, good enough.
+        //todo do this at the event phase
+        return;
+    }
+    else {
+        myStepper->setSpeedInMilliHz(aSpeed);
+    }
+  }
+}
+
+void Stepper::UpdateNormalSpeed(int16_t aSpeed) {
+    myNormalSpeed = aSpeed;
+    UpdateActiveSpeed();
+}
+
+void Stepper::UpdateRapidSpeed(int16_t aSpeed) {
+    myRapidSpeed = aSpeed;
+    UpdateActiveSpeed();
+}
+
+
+
+void Stepper::MoveLeft() {
+    myStepper->runForward();
+}
+
+void Stepper::MoveRight() {
+    myStepper->runBackward();
+}
+
+void Stepper::Stop() {
+    myStepper->stopMove();
+}
+
+void Stepper::SetRapidSpeed() {
+    myUseRapidSpeed = true;
+    UpdateActiveSpeed();
+}
+
+void Stepper::SetNormalSpeed() {
+    myUseRapidSpeed = false;
+    UpdateActiveSpeed();
+}
+
+uint16_t Stepper::GetNormalSpeed() {
+    return myNormalSpeed;
+}
