0435af56d4e545122b12f96cb6eec812926b441d
Chris Chatelain <chris@digiex.ca>
2023-12-12 21:08:24 -0600

    WIP

--- main/stepper.h
+++ main/stepper.h
@@ -11,44 +11,54 @@
 #include <mutex>
 
 class Stepper {
-    public:
+public:
 
     enum class StepperDirection {
         Left = true,
         Right = false
     };
-    //TODO implement mutexes in all of these!!!
-        Stepper();
-        void Init(uint8_t dirPin, uint8_t enablePin, uint8_t stepPin, uint16_t rapidSpeed);
-        void UpdateSpeeds(uint16_t aNormalSpeed, uint16_t aRapidSpeed);
-        void MoveLeft();
-        void MoveRight();
-        void Stop();
-        void SetRapidSpeed();
-        void SetNormalSpeed();
+    Stepper();
+    void Init(uint8_t dirPin, uint8_t enablePin, uint8_t stepPin, uint16_t rapidSpeed);
+    void UpdateSpeeds(uint16_t aNormalSpeed, uint16_t aRapidSpeed);
+    void MoveLeft();
+    void MoveRight();
+    void Stop();
+    void SetRapidSpeed();
+    void SetNormalSpeed();
 
-		uint16_t GetCurrentSpeed() {
-			return myStepper.getSpeed();
-		}
+	/**
+	 **@brief Get the current speed in hz
+	 **@return The current speed in hz
+	 **/
+//#ifdef USE_DENDO_STEPPER
+//	uint16_t GetCurrentSpeed() {
+//		return myStepper.getSpeed();
+//	}
+//#elif USE_FASTACCELSTEPPER
+//	uint16_t GetCurrentSpeed() {
+//		return myStepper->getCurrentSpeedInMilliHz()/1000;
+//	}
+//#endif
 
-		std::string GetState();
 
-		bool IsStopped();
-    private:
+	std::string GetState();
+
+	bool IsStopped();
+private:
         
-        void UpdateActiveSpeed();
+    void UpdateActiveSpeed();
         
-        #ifdef USE_DENDO_STEPPER
-        DendoStepper myStepper;
-        DendoStepper_config_t myStepperCfg;
-        #elif USE_FASTACCELSTEPPER
-        FastAccelStepperEngine myEngine = FastAccelStepperEngine();
-        FastAccelStepper* myStepper;
-        #endif
-        bool myUseRapidSpeed = false;
+#ifdef USE_DENDO_STEPPER
+    DendoStepper myStepper;
+    DendoStepper_config_t myStepperCfg;
+#elif USE_FASTACCELSTEPPER
+    FastAccelStepperEngine myEngine = FastAccelStepperEngine();
+    FastAccelStepper* myStepper;
+#endif
+    bool myUseRapidSpeed = false;
         
-        uint16_t myRapidSpeed;
-        uint16_t myNormalSpeed;
+    uint16_t myRapidSpeed;
+    uint16_t myNormalSpeed;
 };
 
 #endif // STEPPER_H
